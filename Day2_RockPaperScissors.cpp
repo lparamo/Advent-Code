c7b2b4ace258a961feecc5d0de9ed1160b76eb94
Lorea Páramo <lparamo002@ikasle.ehu.eus>
2022-12-02 11:05:05 +0100

    Day 2 part 2

--- Day2_RockPaperScissors.cpp
+++ Day2_RockPaperScissors.cpp
@@ -1,6 +1,5 @@
 //The first column: A for Rock, B for Paper, and C for Scissors
-//The second column, must be what you should play in response: X for Rock, Y for Paper, and Z for Scissors.
-//The total score is the sum of your scores for each round
+//The second column, says how the round needs to end: X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
 /*The score for a single round is the score for the shape you selected(1 for Rock, 2 for Paper, and 3 for Scissors)
 + the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).*/
 #include <iostream>
@@ -12,21 +11,21 @@
 using namespace std;
 int main() {
 	ifstream entrada("inputDay2.txt");
-	int i, j;
+	int i;
 	int n1 = 2500; int n2 = n1*2;
 	char* data; data = new char[n2];
 	int* RoundScores; RoundScores = new int[n1];
-	int TotalScore=0;
+	int TotalScore = 0;
 	for (i = 0; i < n2; i++) entrada >> data[i];
 	for (i = 0; i < n2; i += 2) {
 		if (data[i]=='A') {
 			switch (data[i+1])
 			{
-			case 'X': {RoundScores[i / 2] = 4; 
+			case 'X': {RoundScores[i / 2] = 3; 
 				break; }
-			case 'Y': {RoundScores[i / 2] = 8; 
+			case 'Y': {RoundScores[i / 2] = 4; 
 				break; }
-			case 'Z': {RoundScores[i / 2] = 3; 
+			case 'Z': {RoundScores[i / 2] = 8; 
 				break; }
 
 			default:
@@ -50,11 +49,11 @@ int main() {
 		else {
 			switch (data[i + 1])
 			{
-			case 'X': {RoundScores[i / 2] = 7;
+			case 'X': {RoundScores[i / 2] = 2;
 				break; }
-			case 'Y': {RoundScores[i / 2] = 2 ; 
+			case 'Y': {RoundScores[i / 2] = 6; 
 				break; }
-			case 'Z': {RoundScores[i / 2] = 6; 
+			case 'Z': {RoundScores[i / 2] = 7; 
 				break; }
 
 			default:
